0fa9cb572977be4c8e7b3c9083f03fd174a3cb44
Kausar <2413014@students.universitasmulia.ac.id>
2026-04-20 12:15:24 +0800

    feat: new learn page

---
+++ my-app/app/learn/page.tsx
@@ -0,0 +1,15 @@
+export default function LearnPage(){
+    return(
+        <div>
+            <div>
+                <img src="propeller.svg" alt="Design" />
+                <p>Design</p>
+            </div>
+<div>
+    <p>Figma</p>
+    <p>8 Hours ago</p>
+</div>
+        </div>
+    )
+
+}
